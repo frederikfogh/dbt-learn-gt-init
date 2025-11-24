3f5a5c79f6f1f45abdb76f5b9c1052113cc94cd5
Frederik Cursor <fredefogh@live.dk>
2025-11-24 11:58:07 +0100

    Included test on staging level

--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -1,5 +1,9 @@
     select
         id as order_id ,
         customer as customer_id,
-        ordered_at as order_date
+        ordered_at as order_date,
+        store_id as store_id,
+        subtotal as order_subtotal,
+        tax_paid as tax_paid,
+        order_total as order_total        
     from {{source('jaffle_shop', 'raw_orders')}}
